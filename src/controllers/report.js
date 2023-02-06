341e025e113c34191fba09e9dc34a62de165d9ea
Adrian Esqueiros <u291718169@upc.edu.pe>
2023-02-06 09:17:09 -0500

    endpoint general report

--- src/controllers/report.js
+++ src/controllers/report.js
@@ -13,8 +13,7 @@ const getGraphicReport = async (req, res) => {
 }
 const getGeneralReport = async (req, res) => {
   connection.query(
-    'select d.district_name, p.surface_area ,p.birth_rate ,p.death_rate  ,p.population ,lq.level_education ,lq.quality_food ,lq.socioeconomics, year(y.year_date ) as year, count(*) as total_denuncias, CAST(AVG(r.rate) AS UNSIGNED) as promedio, c.category_name , CONCAT(        ROUND(( (select  CAST(AVG(r.rate) AS UNSIGNED) as promedio) / 10 * 100 ), 2), "%") AS porcentaje from districts d left join reports r on r.district_id  = d.id left join categories c on c.id = r.category_id left join populations p on p.district_id =d.id  left join life_qualities lq ON lq.district_id  = d.id left join district_years dy on dy.district_id = d.id left join years y on y.id = dy.year_id  where dy.year_id  = r.year_id and p.year_id =r.year_id  and lq.year_id =r.year_id  and d.id =? and r.year_id =? GROUP BY r.category_id;',
-    [req.body.districtId, req.body.yearId],
+    'select count(r.id)as total, r.year_id as year, r.district_id as district, round(AVG(r.rate),2)  as avg_rate, concat(round(( AVG(r.rate)  / 10 * 100 ),2), "%") AS porcentaje from reports r	 group by r.year_id, r.district_id;',
     function (err, results, fields) {
       res.json(results)
       console.log(results) // results contains rows returned by server
--- src/routes/report.js
+++ src/routes/report.js
@@ -4,6 +4,6 @@ const { getGraphicReport, getGeneralReport } = require('../controllers/report')
 const router = express.Router()
 
 router.post('/getGraphicReport', getGraphicReport)
-router.post('/getGeneralReport', getGeneralReport)
+router.get('/getGeneralReport', getGeneralReport)
 
 module.exports = router
